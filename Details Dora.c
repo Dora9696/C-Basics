3f49ec0d307ea821c9946d29408ab193a5e52d49
Dora9696 <92906226+Dora9696@users.noreply.github.com>
2021-10-21 10:30:46 +0530

    Add files via upload

---
+++ Details Dora.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("Name:Dora\nRoll.No:20MH5A0347\nBranch:Mechanical\nCollege:Aditya College of Engineering");
+}
---
+++ Hello World Dora.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("D\nO\nR\nA\n");
+}
